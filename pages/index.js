12564b8a742bab86b69f6322ceb08801890926ef
Vannelo <work@vannelo.com>
2023-10-29 22:46:15 -0600

    Add video

--- pages/index.js
+++ pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
   const [scrolled, setScrolled] = useState(false); // Para detectar scrolleo
   const [isMailSent, setIsMailSent] = useState(false);
   const [sendingMail, setSendingMail] = useState(false);
+  const [isPopupOpen, setIsPopupOpen] = useState(true);
 
   // Detectando scrolleo
   useEffect(() => {
@@ -656,6 +657,35 @@ export default function Home() {
       <footer className={styles.footer}>
         Alex Hoyer 2023® Todos los derechos reservados
       </footer>
+      {isPopupOpen && (
+        <div className={styles.popupVideo}>
+          <button
+            className={styles.closePopup}
+            onClick={() => setIsPopupOpen(false)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="2rem"
+              height="2rem"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+          </button>
+          <div className={styles.popupVideoContainer}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/akOcZ3e454k?si=0kimH1Q7zOL0Bseg"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+      )}
     </>
   );
 }
